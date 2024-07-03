ad745af8f19eded36948c357c67b67fc255fe9fb
Jesse Lindloff <jbenlind@gmail.com>
2024-07-03 10:49:58 -0500

    chore: setting up routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-
-import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Home from "./Pages/Home"
 
 function App() {
-  return (
-    <div className="App">
-      <h1>
-        Hello
-
-      </h1>
-    </div>
-  );
+ return (
+   <div className="App">
+     <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Home />} />
+       </Routes>
+     </BrowserRouter>
+   </div>
+ )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+
+// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// );
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <App />
 );
-
 
